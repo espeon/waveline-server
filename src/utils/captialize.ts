f5de68a3a0ed050c33c4895113062685af882ca0
Emma <brocklix@icloud.com>
2020-09-07 04:23:23 -0500

    ok it's not working, remove autocap

--- src/utils/captialize.ts
+++ src/utils/captialize.ts
@@ -1,9 +1,3 @@
 export const capitalize = (string: string) => {
-	let autocap: boolean
-	if(process.env.AUTO_CAP === "false") autocap = false
-    	else autocap = true
-	return string && autocap == false ? string.toLowerCase()
-		.split(" ")
-		.map((s: string) => s.charAt(0).toUpperCase() + s.substring(1))
-		.join(" ") : string;
+	return string
 };
