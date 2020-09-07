9e1469fbffa6ea43930da431ca1848d502ab297b
Emma <brocklix@icloud.com>
2020-09-07 02:05:45 -0500

    i hope auto_cap works now/??

--- src/utils/captialize.ts
+++ src/utils/captialize.ts
@@ -1,5 +1,8 @@
 export const capitalize = (string: string) => {
-	return string && process.env.AUTO_CAP == false ? string.toLowerCase()
+	let autocap: boolean
+	if(process.env.AUTO_CAP === "false") autocap = false
+    	else autocap = true
+	return string && autocap == false ? string.toLowerCase()
 		.split(" ")
 		.map((s: string) => s.charAt(0).toUpperCase() + s.substring(1))
 		.join(" ") : string;
